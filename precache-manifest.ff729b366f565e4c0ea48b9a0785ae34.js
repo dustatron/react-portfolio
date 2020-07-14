900391dc56c4db2477969d55350f5419691e9157
Dusty McCord <Dustymccord@gmail.com>
2020-07-14 16:41:06 -0700

    Updates

---
+++ precache-manifest.ff729b366f565e4c0ea48b9a0785ae34.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37eda4449c2c51837c4befaf90533731",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "e42985dcc78a48191225",
+    "url": "/react-portfolio/static/css/main.569faeeb.chunk.css"
+  },
+  {
+    "revision": "a25f372b080a2228c18c",
+    "url": "/react-portfolio/static/js/2.bd80896f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.bd80896f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e42985dcc78a48191225",
+    "url": "/react-portfolio/static/js/main.b4e80aeb.chunk.js"
+  },
+  {
+    "revision": "90985fadcaf169eb89e6",
+    "url": "/react-portfolio/static/js/runtime-main.94090f38.js"
+  },
+  {
+    "revision": "06e1c5640e183dc62ec42f642d211011",
+    "url": "/react-portfolio/static/media/blazers-02.06e1c564.png"
+  },
+  {
+    "revision": "315b071006b713b8cbd6ff19868402fc",
+    "url": "/react-portfolio/static/media/blazers.315b0710.png"
+  },
+  {
+    "revision": "a3cffe0be8b9a369ef7958f6739c17b4",
+    "url": "/react-portfolio/static/media/dev-connect.a3cffe0b.png"
+  },
+  {
+    "revision": "62feb394b904e4503820acdad5ad4bdb",
+    "url": "/react-portfolio/static/media/github-simple.62feb394.svg"
+  },
+  {
+    "revision": "d1b2ae9bdb33a0acb70d85ee63a32c87",
+    "url": "/react-portfolio/static/media/header-1.d1b2ae9b.png"
+  },
+  {
+    "revision": "6799992f951a71a98decc6e471350995",
+    "url": "/react-portfolio/static/media/karaoke.6799992f.png"
+  },
+  {
+    "revision": "4d88a7c3265c529a08c37c1506adf832",
+    "url": "/react-portfolio/static/media/linkedin.4d88a7c3.svg"
+  },
+  {
+    "revision": "a930f74fff918f159c7b0fbc28b90f3e",
+    "url": "/react-portfolio/static/media/mailbox.a930f74f.svg"
+  },
+  {
+    "revision": "82c2cc97b7ded8621012b38d248b4078",
+    "url": "/react-portfolio/static/media/next-arrow.82c2cc97.svg"
+  },
+  {
+    "revision": "1629a51674b383634e2ade78f02b93fd",
+    "url": "/react-portfolio/static/media/pfv.1629a516.png"
+  },
+  {
+    "revision": "e65b194bb9e7c297aacf11a1518138df",
+    "url": "/react-portfolio/static/media/prev-arrow.e65b194b.svg"
+  },
+  {
+    "revision": "6a90580c2a73a3f69e9f5752c5a751d4",
+    "url": "/react-portfolio/static/media/tech-stuff-1.6a90580c.svg"
+  },
+  {
+    "revision": "7d2f02433c48a2dd92b6938f36c06f39",
+    "url": "/react-portfolio/static/media/ts.7d2f0243.png"
+  },
+  {
+    "revision": "6ac7c095faa667eaea8d2d108990229e",
+    "url": "/react-portfolio/static/media/twitter.6ac7c095.svg"
+  },
+  {
+    "revision": "020bfc81d1ee751167c55763afd5c461",
+    "url": "/react-portfolio/static/media/ve.020bfc81.png"
+  }
+]);
